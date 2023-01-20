d2357ef61ad0bce5cf9b31dc890a6fdb23daac17
EarthmanAman <hashimathman@gmail.com>
2023-01-20 16:35:43 +0300

    complete1

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from . models import Case, Category
 
 def index(request):
     template_name = "./index.html"
-    return render(request, template_name)
+    total = Case.objects.filter()
+    active = total.filter(closed=False)
+    resolved = total.filter(closed=True)
+    # pending = total.filter(status="p")
+    categories = Category.objects.all()
+
+    context = {
+        "active":active.count(),
+        "resolved":resolved.count(),
+        "pending": active.count(),
+        "total": total.count(),
+        "latest": total.order_by("-pk")[:10],
+        "categories": categories
+    }
+    return render(request, template_name, context)
