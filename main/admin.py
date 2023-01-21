6165860ed3ae310c0ee0ce7ec7a54ae817c93209
EarthmanAman <hashimathman@gmail.com>
2023-01-21 12:34:51 +0300

    Images

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from . models import Category, Case
+from . models import Category, Case, Media
 
 admin.site.register(Category)
 admin.site.register(Case)
+admin.site.register(Media)
 
