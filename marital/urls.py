de437195ccb1e5eab1c4473142f6c431e7a46ca5
EarthmanAman <hashimathman@gmail.com>
2023-02-10 11:21:54 +0300

    main url update

--- marital/urls.py
+++ marital/urls.py
@@ -4,14 +4,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Customizing the djanfo admin portal
 admin.site.site_header = "MyPlan Admin Site"
 admin.site.site_title = "MyPlan Admin"
 admin.site.index_title = "Welcome to Admin Panel"
 
+# Main urls
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("", include("main.urls", namespace="main")),
-    path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("users/portal/", include("portal.urls", namespace="portal")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("", include("main.urls", namespace="main")), # To main app urls
+    path("accounts/", include("accounts.urls", namespace="accounts")), # To account app urls
+    path("users/portal/", include("portal.urls", namespace="portal")), # To portal app urls
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Linking static and media files
