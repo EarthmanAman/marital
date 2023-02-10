dc3b563ae29a3638c549dc613bfb8f034db20c22
EarthmanAman <hashimathman@gmail.com>
2023-02-10 09:50:42 +0300

    accounts model update

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    """
+    Creating a user model by inheriting the Django standard user and adding 
+    Phone number and address as required field during user login
+    """
     phone_number = models.CharField(max_length=15, blank=True)
     address  = models.CharField(max_length=100, blank=True)
